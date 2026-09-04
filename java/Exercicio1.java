890988cb404dbb977e031e20ba5e7a8edfdb1161
João Paulo <joaopaulolazarodeoliveira@gmail.com>
2026-09-04 13:46:10 -0300

    adiciona exercicio 1 em java

---
+++ java/Exercicio1.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Exercicio1 {
+
+    public static void main(String[] args){
+
+        double temperatura;
+
+        Scanner leia = new Scanner(System.in);
+
+        System.out.println("Digite a temperatura: ");
+
+        temperatura = leia.nextDouble();
+
+        if (temperatura>= 28){
+              System.out.println(" Ligar ar-condicionado.");
+        }
+         else{
+             System.out.println("Temperatura agradável!. ");
+        }
+    }
+}
