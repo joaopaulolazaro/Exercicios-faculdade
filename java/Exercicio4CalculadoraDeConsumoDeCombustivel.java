35ceaffa3b7c8bb18688f58b842bfbccab742bd7
João Paulo <joaopaulolazarodeoliveira@gmail.com>
2026-09-05 22:01:51 -0300

    Exercício 4 calculadora de consumo de combustível (Java)

---
+++ java/Exercicio4CalculadoraDeConsumoDeCombustivel.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Exercicio4CalculadoraDeConsumoDeCombustivel {
+
+    static void main() {
+        double distancia, consumo, preco, litros_gastos, custo_total;
+
+        Scanner leia = new Scanner(System.in);
+
+        System.out.println(" Digite a distancia percorrida (em km) ");
+        distancia = leia.nextDouble();
+
+        System.out.println(" Digite o consumo do carro (em km/l) ");
+        consumo = leia.nextDouble();
+
+        System.out.println(" Digite o preco do combustivel (por litro) ");
+        preco = leia.nextDouble();
+
+        litros_gastos = distancia / consumo;
+        custo_total = litros_gastos * preco;
+
+        System.out.println(" litros gastos: " + litros_gastos );
+        System.out.println(" Custo total: " + custo_total );
+
+    }
+}
