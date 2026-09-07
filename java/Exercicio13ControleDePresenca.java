dc53ef1d263aafc47f9cb1ce4ee3b538cf3ba924
João Paulo <joaopaulolazarodeoliveira@gmail.com>
2026-09-07 13:13:39 -0300

    Exercício 13 controle de presença (java)

---
+++ java/Exercicio13ControleDePresenca.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Exercicio13ControleDePresença {
+
+    public static void main(String[] args){
+
+        int aulas, faltas;
+        double frequencia;
+
+        Scanner leia = new Scanner(System.in);
+
+        System.out.println("Digite a quantidade de aulas: ");
+        aulas = leia.nextInt();
+
+        System.out.println("Digite a quantidade de faltas: ");
+        faltas = leia.nextInt();
+
+        frequencia = ((aulas - faltas) * 100) / aulas;
+
+        if (frequencia >= 75){
+            System.out.println("Frequencia suficiente.");
+        }
+        else {
+            System.out.println("Frequencia insuficiente.");
+        }
+
+        }
+
+
+    }
+
